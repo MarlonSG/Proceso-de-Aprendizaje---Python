fbaea37b4cf77e9d7ed288124a40f7bc79565bc8
MarlonSG <marlonsg220@gmail.com>
2025-01-24 21:52:08 -0500

    update

--- Conceptos - Python/herencia.py
+++ Conceptos - Python/herencia.py
@@ -9,6 +9,9 @@ class Perro(Animal):
     def hacer_sonido(self):
         print('Puedo ladrar')
 
+    def dormir(self): # Modificar Dormir
+        print('Duermo 15 Horas')
+
 # Programa principal
 print('*** Ejemplo de Herencia en Python ***')
 print('Clase Padre, soy un Animal')
@@ -19,5 +22,5 @@ animal1.dormir()
 print('\nClase Hija, soy un Perro')
 perro1 = Perro()
 perro1.comer()
-perro1.dormir()
+perro1.dormir()  #SE LLAMA A LA MODIFICACION COMO UNA NORMAL
 perro1.hacer_sonido()
